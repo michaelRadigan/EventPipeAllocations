aaee376593a99770b6117ba324b757d287d96840
Michael Radigan <michael@radigan.co.uk>
2022-02-17 13:42:30 +0000

    Add proof of concept for monitoring the current application in a task

--- EventPipeAllocations/Program.cs
+++ EventPipeAllocations/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace EventPipeAllocations
 {
@@ -9,7 +11,41 @@ namespace EventPipeAllocations
         {
             var process = Process.GetCurrentProcess();
             //RuntimeGCEventsPrinter.PrintRuntimeGCEvents(5968);
-            var bar = TraceLogStackTracePrinter.PrintBasicStackTraces(7645).Result;
+            // We could always start the eventListener as a sidecar process?
+            
+            
+            var currentProcessId = Process.GetCurrentProcess().Id;
+            Task.Factory.StartNew(() => TraceLogStackTracePrinter.PrintBasicStackTraces(currentProcessId).Result);
+
+            var i = 0;
+            while (i < 3)
+            {
+                Allocate10K();
+                Allocate5K();
+                GC.Collect();
+                Console.WriteLine(i);
+                Thread.Sleep(5000);
+                i += 1;
+            }
+            //Console.WriteLine(task.Result);
+            
+            
+            
+        }
+        private static void Allocate10K()
+        {
+            for (int i = 0; i < 10000; i++)
+            {
+                int[] x = new int[100];
+            }
+        }
+
+        private static void Allocate5K()
+        {
+            for (int i = 0; i < 5000; i++)
+            {
+                int[] x = new int[100];
+            }
         }
     }
 }
